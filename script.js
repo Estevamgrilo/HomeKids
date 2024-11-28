5487746a34332b24fa63c21375a4933f007c3e44
Estevamgrilo <estevamgrilo@gmail.com>
2024-11-28 16:34:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+document.getElementById("username").textContent = "João"; // Exemplo de nome de usuário
+
+// Simular tarefas no calendário
+let tasks = [
+  { day: 'Segunda-feira', task: 'Escovar os dentes', points: 10 },
+  { day: 'Terça-feira', task: 'Lavar a louça', points: 15 }
+];
+
+let tasksDiv = document.getElementById("tasks");
+tasks.forEach(task => {
+  let taskElement = document.createElement("div");
+  taskElement.innerHTML = `<p>${task.day}: ${task.task} - <strong>${task.points} Migux</strong></p>`;
+  tasksDiv.appendChild(taskElement);
+});
